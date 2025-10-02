d46817d59853bb014c1cc09668ccd3a9c8aba0a1
Thanh Sang <thanhsanggl06@gmail.com>
2025-10-02 22:07:41 +0700

    demo

---
+++ Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace BlazorSolution.Model
+{
+    public class Product : ObservableValidator
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [MinLength(2, ErrorMessage = "Tên phải ít nhất 2 ký tự")]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public void Validate()
+        {
+            ValidateAllProperties();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+using BlazorSolution;
+using BlazorSolution.Model;
+using BlazorSolution.MsgBox;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after"); 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IMessageBoxService, MessageBoxService>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
